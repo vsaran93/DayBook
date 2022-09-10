48299e8d9b4e24f8c03ba09bbad203f93fdfe714
vsaran93 <vishnusaran37@gmail.com>
2022-09-11 00:36:55 +0800

    intial commit

---
+++ DayBook/Controllers/TransactionController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DayBook.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateTransaction([FromBody] Transaction transaction)
+        {
+            try
+            {
+                return Ok("Transaction");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetTransactions()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTransactionById()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTransaction(int id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return BadRequest("Id is missing");
+                }
+                return Ok();
+
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DayBook/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DayBook.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime TransactionDate { get; set; }
+        public double Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
